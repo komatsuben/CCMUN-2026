a175c054abf211737b1b74e3e1344fa90294c5b3
komatsuben <benedicthalim1@gmail.com>
2025-06-07 13:34:24 +0800

    gh page host config changes

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CCMUN-2026/', // Replace with your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
